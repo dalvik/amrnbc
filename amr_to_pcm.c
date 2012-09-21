d221ed5d4ad36ad4d24576d0624027c013ed91cc
dalvik <sky.xctc@163.com>
2012-09-21 14:44:07 +0800

    resolve decode amr exception

--- amr_to_pcm.c
+++ amr_to_pcm.c
@@ -42,39 +42,37 @@ JNIEXPORT jint JNICALL Java_com_iped_ipcam_gui_UdtTools_initAmrDecoder(JNIEnv *e
 	pthread_mutex_init(&amr_decode_lock,NULL);
 	if (Speech_Decode_Frame_init(&speech_decoder_state, "Decoder"))
       		return -1;
+	LOGI("### UdtTools initAmrDecoder success");
 	return 0;
 }
 
 JNIEXPORT jint JNICALL Java_com_iped_ipcam_gui_UdtTools_amrDecoder(JNIEnv *env, jobject thiz,jbyteArray src,jint src_size,jbyteArray dst,jint dst_size,jint channels) 
 {
+ 	 // LOGI("### 11111");
 	  Word16 serial[SERIAL_FRAMESIZE];   /* coded bits                    */
 	  Word16 synth[L_FRAME];             /* Synthesis                     */
 	  Word32 frame;
-
 	  enum Mode mode = (enum Mode)0;
 	  enum RXFrameType rx_type = (enum RXFrameType)0;
-	     
 	  Word16 reset_flag = 0;
 	  Word16 reset_flag_old = 1;
 	  Word16 i;
 	  int x;
-	  
+ 	  //LOGI("### 22222");	  
 	  UWord8 toc, q, ft;
 	  UWord8* psrc;
 	  Word16* pdst;
 	  UWord8 packed_bits[MAX_PACKED_SIZE];
 	  Word16 packed_size[16] = {12, 13, 15, 17, 19, 20, 26, 31, 5, 0, 0, 0, 0, 0, 0, 0};
 	  dst_size=0;
-
 	  frame = 0;
 	  jboolean isCopy = 1; 
 	  char* srcData = (char*)(*env)->GetByteArrayElements(env,src, &isCopy); 
-	  (*env)->ReleaseByteArrayElements(env, src, srcData, 0); 
 	  isCopy = 1;
 	  char* desData = (char*)(*env)->GetByteArrayElements(env,dst, &isCopy); 
-	  (*env)->ReleaseByteArrayElements(env, dst, desData, 0); 
 	  psrc=(UWord8*)srcData;
 	  pdst=(Word16*)desData;
+ 	 // LOGI("### 66666");
 //LOGI("des data length %d",(*env)->GetArrayLength(env,dst));
 	  //pthread_mutex_lock(&amr_decode_lock);
 	  while (src_size>0/*fread (&toc, sizeof(UWord8), 1, file_serial) == 1*/)
@@ -93,7 +91,6 @@ JNIEXPORT jint JNICALL Java_com_iped_ipcam_gui_UdtTools_amrDecoder(JNIEnv *env, 
 		  psrc+=packed_size[ft];
 		  src_size-=sizeof(UWord8)*packed_size[ft];
 		  /*fread (packed_bits, sizeof(UWord8), packed_size[ft], file_serial);*/
-
 		  rx_type = UnpackBits(q, ft, packed_bits, &mode, &serial[1]);
 /*		    
 		  ++frame;
@@ -106,7 +103,6 @@ JNIEXPORT jint JNICALL Java_com_iped_ipcam_gui_UdtTools_amrDecoder(JNIEnv *env, 
 	          } else {
 	            speech_decoder_state->prev_mode = mode;
 	          }
-
 		     /* if homed: check if this frame is another homing frame */
 		     if (reset_flag_old == 1)
 		     {
@@ -135,7 +131,6 @@ JNIEXPORT jint JNICALL Java_com_iped_ipcam_gui_UdtTools_amrDecoder(JNIEnv *env, 
 				pdst+=channels;
 			 }
 			 dst_size+=L_FRAME; 
-
 		     /*
 		     if (fwrite (synth, sizeof (Word16), L_FRAME, file_syn) != L_FRAME) {
 			 fprintf(stderr, "\nerror writing output file: %s\n",
@@ -151,15 +146,17 @@ JNIEXPORT jint JNICALL Java_com_iped_ipcam_gui_UdtTools_amrDecoder(JNIEnv *env, 
 			 reset_flag = decoder_homing_frame_test(&serial[1], mode);
 		     }
 		     /* reset decoder if current frame is a homing frame */
-
 		     if (reset_flag != 0)
 		     {
 			 Speech_Decode_Frame_reset(speech_decoder_state);
 		     }
 		     reset_flag_old = reset_flag;
-
-	  }
-	//pthread_mutex_unlock(&amr_decode_lock);
+	 }
+	 //LOGI("### 77777");
+ 	(*env)->ReleaseByteArrayElements(env, src, srcData, 0); 
+	 //LOGI("### 88888");
+	(*env)->ReleaseByteArrayElements(env, dst, desData, 0); 
+	// LOGI("### 99999");
 	return dst_size;
          
 }
